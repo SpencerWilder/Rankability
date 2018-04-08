97accb7b5e939e9346b210f364fdb509afc609b5
Spencer Wilder <wildersb@g.cofc.edu>
2018-04-07 20:44:13 -0400

    Fixed EO

--- EvolutionaryOptimization/EO.m
+++ EvolutionaryOptimization/EO.m
@@ -1,79 +1,90 @@
 %Evolutionary Optimization 1.0
 %
 tic
-D = [1,1,1,1,1,1;
-     1,0,1,1,0,1;
-     0,0,0,0,0,1;
-     1,0,1,0,1,0;
-     0,1,1,0,0,0;
-     1,0,0,1,1,0];
+% D = [1,1,1,1,1,1;
+%      1,0,1,1,0,1;
+%      0,0,0,0,0,1;
+%      1,0,1,0,1,0;
+%      0,1,1,0,0,0;
+%      1,0,0,1,1,0];
+D=[0 1 1 0 1 1;
+   0 0 1 1 0 0;
+   0 1 0 0 0 0;
+   1 0 0 0 0 1;
+   1 0 1 0 0 0;
+   0 1 1 0 1 0];
 
 %Collect information about data
 [numRows, numCols] = size(D);
 perm = [1:numRows];
 maxP = factorial(numRows);
-n=length(D);
-sizePopulation = 10;
+populationSize = 10;
 maxk = numRows*numRows - numRows;
 
 %First: Generate a population (i.e. generate rankings)
 %NOTE: There are numRows! possible rankings
 %Write a for loop using randperm for get a more random initial population
-firstGen = [];
-for i = 1:sizePopulation
+population = [];
+for i = 1:populationSize
     perm = randperm(numRows);
-    firstGen = [firstGen perm'];
+    population = [population perm'];
 end
-firstGen;
 
 %Second: Calculate fitness
 %As of now, fitness = k
 %Need to find a way to keep track of permuatations and their fitness
-firstGenFit = [];
-%op=[]
- for i = 1:length(firstGen)
-    % perm=nextperm(perm,i);
-     perm = firstGen(:,i);
+fitArray = [];
+for i = 1:populationSize
+     perm = population(:,i);
      A = D(perm, perm);
      fitness=nnz(tril(A))+(numRows*(numRows-1)/2 - nnz(triu(A,1)));
-     firstGenFit = [firstGenFit; fitness];
-     %op = [op; i, fitness]
- end
-%B = [sortrows(op, 2)]
+     fitArray = [fitArray fitness];
+end
+[fitArray, indices] = sort(fitArray);
+population = population(:, indices);
 
 iteration = 1;
-kArray = [];
-while k < previousk || iteration <15
-  if iteration == 1
-    previousk = 100000000;
-  else
-    previousk = k;
-  end
-%Third: Selection
-%Probability of choosing a member is proportional to its fitness
-  matingArray = [];
-  for i= 1:sizePopulation
-    [mate1, mate2] = selection(firstGenFit,firstGen,sizePopulation);
-    matingArray = [matingArray mate1 mate2];
+previousk = 100000000;
+while iteration < 50 %k < previousk || iteration <15
+
+  %Fourth: Mating
+  for i = 1:ceil(populationSize/2)
+      for j = i:ceil(populationSize/2)
+        offspring = leftTieBreakMate(population(:,i), population(:,j));
+        population = [population offspring];
+      end
   end
 
-%Fourth: Mating
-  offspringArray = [];
-  for i = 1:length(matingArray)/2
-    offspring = leftTieBreakMate(matingArray(:,i),matingArray(:,length(matingArray)/2+i));
-    offspringArray = [offspringArray offspring];
+  %Fifth: Mutating remaining offspring
+  for i = ceil(populationSize/2)+1:populationSize
+    offspring = flipMutate(population(:,i));
+    population = [population offspring];
   end
 
-%Fifth: Fitness of offspring
-  newFitArray = [];
-  for i = 1:length(offspringArray)
-      R = D(offspringArray(i),offspringArray(i));
+  %Fifth: Fitness of offspring
+  fitArray = [];
+  for i = 1:length(population)
+      R = D(population(:,i),population(:,i));
       newFit =nnz(tril(R))+(numRows*(numRows-1)/2 - nnz(triu(R,1)));
-      newFitArray = [newFitArray newFit];
+      fitArray = [fitArray newFit];
   end
 
-  k = min(newFitArray);
-  firstGenFit = newFitArray';
-  firstGen = offspringArray;
+  [fitArray, indices] = sort(fitArray);
+  population = population(:,indices);
+  population = unique(population', 'rows', 'stable');
+  population = population';
+
+
+  %select most fit
+  populationSize
+  if populationSize <= length(population)
+    population = population(:,[1:populationSize]);
+    fitArray = fitArray(1:populationSize);
+  end
+
+  %take unique
+  k = min(fitArray);
+  iteration = iteration + 1;
 end
+k
 toc
